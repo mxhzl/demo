6dd48d746499905a53f99aa15628edbbc696eabd
Maxine <krebs.maxine@gmail.com>
2026-02-11 14:33:28 -0500

    Use root dns instead of subdomain

--- demo_infra/terraform/dns.tf
+++ demo_infra/terraform/dns.tf
@@ -1,14 +1,28 @@
-resource "porkbun_dns_record" "demo" {
+resource "porkbun_dns_record" "mxhzldotcom" {
   domain   = "mxhzl.com"
-  name     = "demo"
   type     = "A"
   content  = digitalocean_reserved_ip.demo.ip_address
   priority = 0
 }
 
-resource "porkbun_dns_record" "ipv6-demo" {
+resource "porkbun_dns_record" "ipv6-mxhzldotcom" {
   domain   = "mxhzl.com"
-  name     = "demo"
+  type     = "AAAA"
+  content  = digitalocean_droplet.demo.ipv6_address
+  priority = 0
+}
+
+resource "porkbun_dns_record" "www" {
+  domain   = "mxhzl.com"
+  name     = "www"
+  type     = "A"
+  content  = digitalocean_reserved_ip.demo.ip_address
+  priority = 0
+}
+
+resource "porkbun_dns_record" "ipv6-www" {
+  domain   = "mxhzl.com"
+  name     = "www"
   type     = "AAAA"
   content  = digitalocean_droplet.demo.ipv6_address
   priority = 0
